6951967fe41b122fbc0e74c9b474459277ce9a34
Nico <121408070+nico-policzer@users.noreply.github.com>
2023-02-04 15:37:24 -0800

    Add Task, add toStr in event

--- src/Event.py
+++ src/Event.py
@@ -2,11 +2,22 @@ class Event:
     startTime = 0
     duration = 0
     endTime = 0
-
+    name = ""
+    description = ""
 
     # Create an event starting at startTime minutes from midnight, that is duration minutes long
-    def __init__(self, startTime, duration) -> None:
+    def __init__(self, name, description, startTime, duration) -> None:
         self.startTime = startTime
         self.duration = duration
         self.endTime = duration + startTime
+        self.name = name
+        self.description = description
+
+    
+    def __str__(self) -> str:
+        retStr = self.name +  "\n" + self.description
+
+    def to24Time(self) -> str:
+        return self.duration // 60 + " : " + self.duration % 60
+        
 
