2fdc63bed4d5728a0ffd308abc9cb4c7c1c2af73
ChaoticArmadillo1 <96665523+ChaoticArmadillo1@users.noreply.github.com>
2023-02-04 16:17:31 -0800

    Added functionality to Date class

--- src/Day.py
+++ src/Day.py
@@ -1,17 +1,46 @@
+import Event
 class Day:
-    events = None
+    events = []
+    tasks = []
     schedule = []
     interval = 15  # how many minutes each index in an array takes
 
-    def __int__(self, events):
+    # Require: events is a list of events
+    #           tasks are a list of tasks
+    def __int__(self, events, tasks):
         self.events = events
+        self.tasks = tasks
 
-        for i in range(1140 // self.intervald):
+        # creating the
+        for i in range(1140 // self.interval):
             self.schedule.add(True)
+
+        ev: Event
+        for ev in events:
+            self.scheduleEvent(ev)
+
+    def scheduleEvent(self, ev):
+        startTime = ev.startTime
+        duration = ev.duration
+        successful = True
+
+        index = startTime // self.interval  # the start time of the event within our interval
+
+        # This for loop adds the events to the schedule at the correct time
+        # TODO: for future, check error handling for when the event goes over the time
+        for i in range(index, index + duration // self.interval):
+            if type(self.schedule[i]) == bool and self.schedule[i]:
+                self.schedule[i] = ev
+            else:
+                successful = False
+
+        return successful
+
+
+
 
 
     def getNumEvents(self):
         return len(self.events)
 
-    def
 
